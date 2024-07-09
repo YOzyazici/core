555e672bd6b81990ae1b9089e4ce22627a156856
Yasin <yasinzc75@gmail.com>
2024-07-09 19:41:59 +0300

    first commit

---
+++ src/main/java/com/pair/core/entities/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.pair.core.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(name = "update_date")
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    @Column(name = "deleted_date")
+    private LocalDateTime deletedDate;
+}
---
+++ src/main/java/com/pair/core/utils/constants/Paths.java
@@ -0,0 +1,15 @@
+package com.pair.core.utils.constants;
+
+public class Paths {
+    public static final String ConfigurationBasePackage = "com.pair.core";
+
+    public static class Customer {
+        public static final String ServiceBasePackage = "com.pair.customerService";
+    }
+
+    public static class Auth {
+        public static final String ServiceBasePackage = "com.pair.authService";
+    }
+
+
+}
